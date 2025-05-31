6c913fd4cc999081be8f81d3983719b6d540c179
mahmoudjd <mahmoudjd452@gmail.com>
2025-05-31 13:03:31 +0200

    fix: update image link from playmakerstats

--- server/src/scraper/extractPlayMarker.ts
+++ server/src/scraper/extractPlayMarker.ts
@@ -215,7 +215,7 @@ export const extractDataPlaymakerstats = async (
       number: parseInt(number.toString().replace(".", "")) || 0,
       fullName,
       currentClub,
-      image,
+      image: "https://www.playmakerstats.com"+image,
       caps,
       birthCountry,
       value: "",
