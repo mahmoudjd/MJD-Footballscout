009b511293e50123a8166aa9b11ca61b8d75375e
mahmoudjd <mahmoudjd452@gmail.com>
2025-06-17 22:06:29 +0200

    fix(players): refactor page to client component to handle live updates on delete

--- web-app/src/app/players/page.tsx
+++ web-app/src/app/players/page.tsx
@@ -1,12 +1,25 @@
-import { fetchPlayers } from "@/lib/hooks/queries/get-players"
+"use client";
+
+import { useEffect, useState } from "react";
+import { fetchPlayers } from "@/lib/hooks/queries/get-players";
 import PlayersList from "@/components/players/players-list";
+import {Loader} from "@/components/loader";
+import PlayerType from "@/lib/types/type";
 
+export default function PlayersPage() {
+    const [players, setPlayers] = useState<PlayerType[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+    useEffect(() => {
+        fetchPlayers().then(data => {
+            setPlayers(data);
+            setIsLoading(false);
+        });
+    }, []);
 
-export default async function PlayersPage() {
-    const players = await fetchPlayers();
+    if (isLoading) {
+        return <Loader />;
+    }
 
     return <PlayersList players={players} />;
 }
--- web-app/src/lib/hooks/queries/get-players.ts
+++ web-app/src/lib/hooks/queries/get-players.ts
@@ -7,10 +7,9 @@ export async function fetchPlayers(): Promise<PlayerType[]> {
         const response = await fetch(`${env.NEXT_PUBLIC_API_HOST}/players`, {
             headers: {
                 "Accept": "application/json"
-            },
-            cache: 'no-store' // Disable caching to ensure fresh data
+            }
         });
-        return await response.json();
+        return (await response.json()) as PlayerType[];
     } catch (error) {
         console.error(error);
         return [];
