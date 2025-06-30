8d04eca57a3788f284d42f54e319bace5ddc8ff6
mahmoudjd <mahmoudjd452@gmail.com>
2025-06-30 22:24:35 +0200

    refactor(scraper): update request headers in `extractBesoccer`

--- server/src/scraper/extractBesoccer.ts
+++ server/src/scraper/extractBesoccer.ts
@@ -25,10 +25,6 @@ const fetchHTML = async (url: string, extraHeaders = {}): Promise<string> => {
 
     const headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Accept': 'text/html',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en-US,en;q=0.9',
-        ...extraHeaders,
     };
 
     try {
