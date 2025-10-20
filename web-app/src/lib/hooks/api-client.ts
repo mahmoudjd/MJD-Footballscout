37187bbc181f40d0047a1e9d3dfc9adf41b98874
mahmoudjd <mahmoudjd452@gmail.com>
2025-10-20 18:45:02 +0200

    fix: use apiClient in the use-planyers hook

--- web-app/src/lib/hooks/api-client.ts
+++ web-app/src/lib/hooks/api-client.ts
@@ -1,21 +1,21 @@
-import axios from "axios"
+import axios from "axios";
 import { env } from "@/env";
-import {getSession} from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 const apiClient = axios.create({
-    baseURL: env.NEXT_PUBLIC_API_HOST
-})
+  baseURL: env.NEXT_PUBLIC_API_HOST,
+});
 
+apiClient.interceptors.request.use(async (config) => {
+  const session = await getSession();
+  const accessToken = session?.user.accessToken;
 
-apiClient.interceptors.request.use(async (config)=> {
-    const session = await getSession()
-    const accessToken= session?.user.accessToken
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
 
-    if (accessToken) {
-        config.headers.Authorization= `Bearer ${accessToken}`
-    }
+  return config;
+});
 
-    return config
-})
+export { apiClient };
 
-export { apiClient }
