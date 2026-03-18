b2eef9e6734e19eb93e71baa832d1eca413361e4
mahmoudjd <mahmoudjd452@gmail.com>
2026-03-18 23:15:33 +0100

    chore(server): remove legacy controllers models and routes

--- server/src/models/objectId.ts
+++
@@ -1,8 +0,0 @@
-import {ObjectId} from "mongodb"
-import {z} from "zod";
-
-export const ObjectIdSchema = z.object({
-    _id: z.custom<ObjectId>((val) => val instanceof ObjectId, {
-        message: "_id must be a valid ObjectId",
-    })
-})
